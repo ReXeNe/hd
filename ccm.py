d9ce8e88745e90c1c8879a80c8da01ef32bbe15a
Chucky <154457844+ReXeNe@users.noreply.github.com>
2024-03-16 09:07:55 +0300

    Create ccm.py

---
+++ ccm.py
@@ -0,0 +1,45 @@
+import requests,random
+import requests,random,os,uuid,json,user_agent,time,sys,socket,datetime
+from datetime import date
+from time import sleep
+from user_agent import generate_user_agent
+from uuid import uuid4
+from os import system
+import os
+import sys
+import time
+import requests
+
+def chk():
+
+
+  os.system('clear')
+
+  uuid = str(os.geteuid())
+  id = "yes"
+  try: 
+    httpCaht = requests.get("رابط").text
+    if id in httpCaht:
+      
+      msg = str(os.geteuid())
+      time.sleep(2)
+      pass
+    else:
+        
+      
+
+      time.sleep(1) 
+
+      sys.exit() 
+
+  except: 
+
+    sys.exit() 
+
+    if name == 'main': 
+
+     print (logo)
+
+     chk() 
+
+chk()
